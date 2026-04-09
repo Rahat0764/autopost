1bd6f5374e59d3f6e59f79e0c6379c918b0c689d
Rahat Ahmed <87570918+Rahat0764@users.noreply.github.com>
2026-04-09 14:24:27 +0600

    Update logs.py

--- api/logs.py
+++ api/logs.py
@@ -1,1 +1,19 @@
-.
+import requests
+import os
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+def handler(request):
+    res = requests.get(
+        f"{SUPABASE_URL}/rest/v1/posts?select=*",
+        headers={
+            "apikey": SUPABASE_KEY,
+            "Authorization": f"Bearer {SUPABASE_KEY}"
+        }
+    )
+
+    return {
+        "statusCode": 200,
+        "body": res.text
+    }
